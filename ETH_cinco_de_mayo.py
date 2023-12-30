42da600fa25d0fb48215cd807e4a6eaf4a1dbebd
Noisk8 <juansk8jaramillo@gmail.com>
2023-12-29 23:25:15 -0500

    Update ETH_cinco_de_mayo.py

--- ETH_cinco_de_mayo.py
+++ ETH_cinco_de_mayo.py
@@ -10,6 +10,13 @@ p1 >> piano(P[0, 2, 4, 7, 9, 11], dur=1/4, oct=5)
 p2 >> piano(P[0, 4, 7], dur=4, oct=4)
 
 
-p3 >> play ("")
+var1 = var([1, 1.1], [32])
+var.switch = var([0, 1], [64])
 
-p4 >>  play ("")
+p1 >> sitar(var.switch, dur=PWhite(4), pan=PWhite(-1, 1), amp=lyfvar([0, 2], switch), room=0.5, formant=var.switch)
+ce >> play('x[xx]', amp=5 * var.switch)
+cy >> bass(dur=PwDur(5 & (3,8)), amp=16)
+d3 >> play('*', dur=var([4], [16]), amp=.2).every(6,'stutter',4,dur=Cycle([3/4,2/4]),pan=(-1,0))
+c2 >> play('a', rate=.25).spread()
+bh >> play('---[--]', chop=0., amp=.3).spread()
+jk >> play('[--]', rate=-.25).spread()
