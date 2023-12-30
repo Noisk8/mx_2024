cfd73a4731d38f520151307b65efface9fb41f83
Noisk8 <juansk8jaramillo@gmail.com>
2023-12-29 23:19:17 -0500

    Update ETH_cinco_de_mayo.py

--- ETH_cinco_de_mayo.py
+++ ETH_cinco_de_mayo.py
@@ -3,10 +3,12 @@ Clock.bpm = 130
 print(SynthDefs)
 
 
+# Define la melodía del piano
+p1 >> piano(P[0, 2, 4, 7, 9, 11], dur=1/4, oct=5)
 
-p1 >>
+# Añade algunos acordes
+p2 >> piano(P[0, 4, 7], dur=4, oct=4)
 
-p2 >>
 
 p3 >> play ("")
 
